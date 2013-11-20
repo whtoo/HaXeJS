8bb2e1dff633a86290e1bf2630b90ee697f4776b
whtoo <whtoo@qq.com>
2013-11-20 14:28:18 +0800

    update for all

--- src/UntityJS.hx
+++ src/UntityJS.hx
@@ -1,12 +1,25 @@
 package ;
+import js.Browser;
+import js.Lib;
 
 class UntityJS {
 	
 	function new(){
+		this.bindToWindow();
+	}
+	
+	function bindToWindow(){
+		js.Lib.alert('bind exec 1');
+		trace("wx");
+		var win = untyped __js__("window");
+		win.untityJS = this;
 		
 	}
 	
-	function toDumpStr(){
-	
-	}
+	public static function main():Void{
+		
+			new UntityJS();
+			js.Lib.alert('bind exec 2');
+		
+	}	
 }
